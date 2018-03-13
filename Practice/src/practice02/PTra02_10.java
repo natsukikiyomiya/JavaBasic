582978925705c8174ff3999a245ac801a0eb9c64
natsukikiyomiya <natsuki10071007@gmail.com>
2018-03-13 15:45:11 +0900

    PTra02_10まで完了

--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -21,6 +21,9 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
-
+         int x = 8;
+         int y = coffee + x * 1 ;
+         int z = snack + x * 2 ;
+         System.out.println(moneyInHand - ((y * 2) +z));
 	}
 }
