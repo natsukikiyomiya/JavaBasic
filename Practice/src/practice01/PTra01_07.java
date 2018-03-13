55e4198e6457f484e1d4b5cfc8de1c624abaa2e2
natsukikiyomiya <natsuki10071007@gmail.com>
2018-03-13 13:03:03 +0900

    PTra01_07まで完了

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -10,35 +10,32 @@ public class PTra01_07 {
 	public static void main(String[] args) {
 
 		// 基本型8種類の変数を宣言します
-		boolean bo;
-		byte by;
-		short s;
-		char c;
-		int i;
-		long l;
-		float f;
-		double d;
-
+		boolean isEat;
+		byte myAge;
+		short text;
+		char gender;
+		int age;
+		long matle;
+		float head;
+		double crow;
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
-
-
+        isEat = false;
+        myAge = 20;
+        text = 47;
+        gender = '男';
+        age = 100;
+        matle = 23000;
+        head = 1;
+        crow = 1.2;
 		// ★ それぞれの変数の中身を出力してください
-
-
-
-
-
-
-
-
-
+        System.out.println(isEat);
+        System.out.println(myAge);
+        System.out.println(text);
+        System.out.println(gender);
+        System.out.println(age);
+        System.out.println(matle);
+        System.out.println(head);
+        System.out.println(crow);
 	}
 }
