23c99f28bb8b5c9d60895841c89ec78319d74b66
natsukikiyomiya <natsuki10071007@gmail.com>
2018-04-05 14:54:55 +0900

    PTra08_03まで完了

--- Practice/src/practice08/PTra08_03.java
+++ Practice/src/practice08/PTra08_03.java
@@ -1,4 +1,5 @@
 package practice08;
+
 /*
  * PTra08_03.java
  *   作成	LIKEIT	2017
@@ -10,10 +11,12 @@ public class PTra08_03 {
 	public static void main(String[] args) {
 
 		// ★ 0～100のランダムな値を取得して、変数numberに代入してください
+		 int number = new java.util.Random().nextInt(101);
 
 
 		// ★ 変数resultを宣言し、メソッドisEvenNumberを（変数number）で呼び出した結果を代入してください
-
+		boolean result = isEvenNumber(number);
+		
 
 		if (result) {
 			System.out.println("偶数です");
@@ -21,6 +24,10 @@ public class PTra08_03 {
 			System.out.println("奇数です");
 		}
 	}
+	
+	
+
+	
 
 	/*
 	 * ★ 以下の仕様でメソッドを作成してください
@@ -31,4 +38,7 @@ public class PTra08_03 {
 	 *
 	 * 引数で渡された内容が偶数ならtrue、奇数ならfalseを返すメソッド
 	 */
+	public static boolean isEvenNumber(int x) {
+			return false;
+		}
 }
